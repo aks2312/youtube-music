e0a0ac36edb89881d269600d74372e821a79c60b
iMro0t <ahbhalgami10@gmail.com>
2019-12-31 22:07:06 +0530

    added pip support

--- src/browser.js
+++ src/browser.js
@@ -1,33 +1,37 @@
 const { ipcRenderer } = require("electron");
 
 ipcRenderer.on("toggle-play-pause", () => {
-	document.querySelector("#play-pause-button").click();
+  document.querySelector("#play-pause-button").click();
 });
 
 ipcRenderer.on("next", () => {
-	document.querySelector(".next-button").click();
+  document.querySelector(".next-button").click();
 });
 
 ipcRenderer.on("previous", () => {
-	document.querySelector(".previous-button").click();
+  document.querySelector(".previous-button").click();
 });
 
 ipcRenderer.on("repeat", () => {
-	document.querySelector(".repeat").click();
+  document.querySelector(".repeat").click();
 });
 
 ipcRenderer.on("shuffle", () => {
-	document.querySelector(".shuffle").click();
+  document.querySelector(".shuffle").click();
 });
 
-// ipcRenderer.on("toggle-pip", () => {
-// 	document.querySelector("video").requestPictureInPicture();
-// });
+ipcRenderer.on("toggle-pip", () => {
+  if (!document.pictureInPictureElement) {
+    document.querySelector("video").requestPictureInPicture();
+  } else {
+    document.exitPictureInPicture();
+  }
+});
 
 setInterval(() => {
-	ipcRenderer.send("player-bar-status", {
-		visible: document.querySelector("ytmusic-player-bar").playerPageOpen_,
-		playing: document.querySelector("ytmusic-player-bar").playing_,
-		repeat: document.querySelector("ytmusic-player-bar").repeatMode_
-	});
+  ipcRenderer.send("player-bar-status", {
+    visible: document.querySelector("ytmusic-player-bar").playerPageOpen_,
+    playing: document.querySelector("ytmusic-player-bar").playing_,
+    repeat: document.querySelector("ytmusic-player-bar").repeatMode_
+  });
 }, 100);
--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@
 const { app, BrowserWindow, Menu, ipcMain } = require("electron");
 const { autoUpdater } = require("electron-updater");
 const { is } = require("electron-util");
-const debug = require("electron-debug");
 const { readFileSync } = require("fs");
 const {
-	ensureOnline,
-	createTray,
-	updateMediaControls
+  ensureOnline,
+  createTray,
+  updateMediaControls
 } = require("./helpers.js");
 const config = require("./config");
 const menu = require("./menu");
@@ -16,92 +15,92 @@ app.setAppUserModelId("com.botallen.youtubemusic");
 
 // It's commented out as it throws an error if there are no published versions.
 if (!is.development) {
-	const FOUR_HOURS = 1000 * 60 * 60 * 4;
-	setInterval(() => {
-		autoUpdater.checkForUpdates();
-	}, FOUR_HOURS);
+  const FOUR_HOURS = 1000 * 60 * 60 * 4;
+  setInterval(() => {
+    autoUpdater.checkForUpdates();
+  }, FOUR_HOURS);
 
-	autoUpdater.checkForUpdates();
+  autoUpdater.checkForUpdates();
 }
 
 // Prevent window from being garbage collected
 let mainWindow;
 
 const createMainWindow = async () => {
-	const win = new BrowserWindow({
-		title: app.name,
-		show: false,
-		width: 1280,
-		height: 800,
-		autoHideMenuBar: true,
-		icon: config.get("icons.default"),
-		webPreferences: {
-			nodeIntegration: true,
-			devTools: false
-		}
-	});
+  const win = new BrowserWindow({
+    title: app.name,
+    show: false,
+    width: 1280,
+    height: 800,
+    autoHideMenuBar: true,
+    icon: config.get("icons.default"),
+    webPreferences: {
+      nodeIntegration: true,
+      devTools: false
+    }
+  });
 
-	win.on("ready-to-show", () => {
-		win.show();
-	});
+  win.on("ready-to-show", () => {
+    win.show();
+  });
 
-	win.on("close", event => {
-		// Dereference the window
-		// For multiple windows store them in an array
-		event.preventDefault();
-		win.blur();
-		if (is.macos) {
-			// On macOS we're using `app.hide()` in order to focus the previous window correctly
-			app.hide();
-		} else {
-			win.hide();
-		}
-	});
+  win.on("close", event => {
+    // Dereference the window
+    // For multiple windows store them in an array
+    event.preventDefault();
+    win.blur();
+    if (is.macos) {
+      // On macOS we're using `app.hide()` in order to focus the previous window correctly
+      app.hide();
+    } else {
+      win.hide();
+    }
+  });
 
-	await win.loadURL("https://music.youtube.com");
+  await win.loadURL("https://music.youtube.com");
 
-	return win;
+  return win;
 };
 
 // Prevent multiple instances of the app
 if (!app.requestSingleInstanceLock()) {
-	app.quit();
+  app.quit();
 }
 
 app.on("second-instance", () => {
-	if (mainWindow) {
-		if (mainWindow.isMinimized()) {
-			mainWindow.restore();
-		}
+  if (mainWindow) {
+    if (mainWindow.isMinimized()) {
+      mainWindow.restore();
+    }
 
-		mainWindow.show();
-	}
+    mainWindow.show();
+  }
 });
 
 app.on("window-all-closed", () => {
-	if (!is.macos) {
-		app.quit();
-	}
+  if (!is.macos) {
+    app.quit();
+  }
 });
 
 app.on("activate", async () => {
-	if (!mainWindow) {
-		mainWindow = await createMainWindow();
-	}
+  if (!mainWindow) {
+    mainWindow = await createMainWindow();
+  }
 });
 
 (async () => {
-	await Promise.all([ensureOnline(), app.whenReady()]);
-	Menu.setApplicationMenu(menu);
-	mainWindow = await createMainWindow();
-	createTray(mainWindow);
-	mainWindow.webContents.executeJavaScript(
-		readFileSync(config.get("browser"), "utf8")
-	);
+  await Promise.all([ensureOnline(), app.whenReady()]);
+  Menu.setApplicationMenu(menu);
+  mainWindow = await createMainWindow();
+  createTray(mainWindow);
+  mainWindow.webContents.executeJavaScript(
+    readFileSync(config.get("browser"), "utf8")
+  );
 })();
 
 ipcMain.on("player-bar-status", (_, status) => {
-	if (is.windows) {
-		updateMediaControls(mainWindow, status);
-	}
+  if (is.windows) {
+    updateMediaControls(mainWindow, status);
+  }
 });
